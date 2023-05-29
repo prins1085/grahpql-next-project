d4501546800d2a7f95694c883c7e2652fd81fb1f
Prins Harkhani <harkhaniprins2001@gmail.com>
2023-05-29 10:58:39 +0530

    connct to graphql and fetch data using graphql query

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+const GET_CHARACTERS = gql`
+  query {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
 
-export default Home
+const CharacterList = () => {
+  const { error, loading, data } = useQuery(GET_CHARACTERS);
+
+  if (error) {
+    return <div>Something Went Wrong!</div>;
+  }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return <div className="flex flex-wrap justify-evenly p-4">
+    {data.characters.results.map((character) => {
+      return (
+        <div className="relative">
+          <img src={character.image} alt="Charater_image" />
+          <div className="absolute top-[90%] text-black bg-blue-200 px-2 rounded-md font-extrabold">{character.name}</div>
+        </div>
+      )
+    })}
+  </div>;
+};
+
+export default CharacterList;
